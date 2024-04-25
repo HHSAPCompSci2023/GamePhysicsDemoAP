90a673d453079662eabe5ea80d6b707c188f90a6
John Shelby <john_shelby@fuhsd.org>
2024-04-25 09:15:20 -0700

    Update Mario.java

--- GamePhysicsDemoAP/src/sprites/Mario.java
+++ GamePhysicsDemoAP/src/sprites/Mario.java
@@ -19,14 +19,33 @@ public class Mario extends Sprite {
 	}
 
 	// METHODS
+	
+	/** 
+	 * Makes mario walk left or right across the window
+	 * @param dir -1 for left, 1 for right
+	 * 
+	 * Lead coder: 
+	*/
 	public void walk(int dir) {
 		
 	}
 
+	/** 
+	 * Makes mario jump up
+	 * 
+	 * Lead coder: 
+	 */
 	public void jump() {
 		
 	}
 
+	/** 
+	 * Makes mario do everything that he should do without any keys being pressed
+	 * (such as fall to the ground)
+	 * @param obstacles Other sprites that mario could collide with
+	 * 
+	 * Lead coder:
+	 */
 	public void act(List<Sprite> obstacles) {
 		
 		x += xVel;
